80fa37bd6e9d88bac492612af23b1d47e4a61f1b
chandan s <chandans7711@gmail.com>
2025-01-05 03:24:07 -0500

    Update settings.py

--- aap/settings.py
+++ aap/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = ['.vercel.app']
 # Application definition
 
 INSTALLED_APPS = [
-    
+    'whitenoise.runserver_nostatic',
     # 'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
